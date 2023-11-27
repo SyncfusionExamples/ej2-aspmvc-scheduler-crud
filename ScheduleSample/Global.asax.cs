b209e9f9e7855cf5ca610979e92ffec51f9c24bd
Swathi Ravi <swathi.ravi@syncfusion.com>
2023-11-27 16:07:18 +0530

    EJ2 MVC Scheduler URL adaptor sample added

--- ScheduleSample/Global.asax.cs
+++ ScheduleSample/Global.asax.cs
@@ -15,7 +15,6 @@ namespace ScheduleSample
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
